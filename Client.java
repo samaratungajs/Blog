60bf2577882df698aefd4113da3e2053d9cbdac8
J.Samaratunga <68687791+samaratungajs@users.noreply.github.com>
2021-02-21 01:28:46 +0530

    create
    1

---
+++ Client.java
@@ -0,0 +1,15 @@
+public class Client {
+    private String ClientId;
+    private String ClientName;
+    private String Address;
+    private Date RegistrationDate;
+    
+    public void storeClient(String ClientName){
+          // store client in database
+    }
+    
+    public void generateClientReport(String ClientName){
+          // generate a report
+    }
+
+}
